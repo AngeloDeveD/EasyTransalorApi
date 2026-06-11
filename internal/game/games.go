fb3ba1a83b8928fa49343abdfcfbde3dd8bd757f
Angelo <da.create.gg@gmail.com>
2026-06-12 00:08:08 +0700

    Добавил post запрос на создание игры

--- internal/game/games.go
+++ internal/game/games.go
@@ -1,9 +1,13 @@
 package game
 
 import (
+	"fmt"
 	"net/http"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type GameCard struct {
@@ -27,6 +31,7 @@ type TranslateCard struct {
 	Version       float32 `json:"version"`
 	PercentReady  float32 `json:"percentReady"`
 	UrlToDownload string  `json:"urlToDownload"`
+	FileSize float32 `json:"fileSize"`
 }
 
 var gameCard = []GameCard{
@@ -51,6 +56,7 @@ var gameInfo = []GameInfo{
 				Version:       1.0,
 				PercentReady:  0.0,
 				UrlToDownload: "url",
+				FileSize: 0.0,
 			},
 		},
 	},
@@ -66,20 +72,104 @@ var gameInfo = []GameInfo{
 				Version:       1.0,
 				PercentReady:  0.0,
 				UrlToDownload: "url",
+				FileSize: 0.0,
 			},
 		},
 	},
 }
 
+type CreateGameRequest struct {
+	Title string `json:"title"`
+}
+
+//GET: получение информации об игре (С информацией о переводах)
 func getGame(c *gin.Context){
 	c.JSON(http.StatusAccepted, gameInfo)
 }
 
+//GET: получение карточки игры (Без информации о переводах)
 func getCard(c *gin.Context){
 	c.JSON(http.StatusOK, gameCard)
 }
 
-func SetupGameRoutes(router *gin.Engine) {
-	router.GET("/games", getGame)
-	router.GET("/cards", getCard)
+//POST: добавление игры
+func addGame(c *gin.Context){
+	//Привязка текстовых данных из формф
+	var req CreateGameRequest
+
+	if err := c.ShouldBind(&req); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//Получение всеё формы
+	form, err := c.MultipartForm()
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Файл обязателен"})
+	}
+
+	//Получение файлов
+	files := form.File["images"]
+
+	if len(files) == 0{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Загрузите хотя бы одно фото"})
+		return
+	}
+
+	if len(files) > 2{
+		c.JSON(http.StatusBadRequest, gin.H{"error" : "Максимум 2 изображения"})
+	}
+
+	allowExts := map[string]bool{".jpg": true, ".jpeg": true, ".png": true, ".webp": true}
+
+	var imagesUrls []string
+
+	for _, file := range files {
+		//Валидация изоброажений
+		ext := strings.ToLower(filepath.Ext(file.Filename))
+		if !allowExts[ext]{
+			c.JSON(http.StatusBadRequest, gin.H{"error" : fmt.Sprintf("Файл %s имеет  недопустимый формат", file.Filename)})
+			return
+		}
+
+		if file.Size > 5<<20{
+			c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Файл %s слишком большой", file.Filename)})
+			return
+		}
+
+		newFileName := uuid.New().String() + ext
+		savePath := filepath.Join("uploads", newFileName)
+
+		if err := c.SaveUploadedFile(file, savePath); err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Не удалось сохранить файл"})
+			return
+		}
+
+		imagesUrls = append(imagesUrls, fmt.Sprintf("/static/%s", newFileName))
+	}
+
+	info := GameCard{
+		ID: int(uuid.New().ID()),
+		Title: req.Title,
+		IconUrl: imagesUrls[0],
+	}
+
+	gameCard = append(gameCard, info)
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Успешно создано",
+		"title": req.Title,
+		"image_urls": imagesUrls,
+	})
+}
+
+func SetupGameRoutes(r *gin.Engine) {
+	r.Static("/static", "./uploads")
+
+	/* /games */
+	r.POST("/games/add", addGame)
+	r.GET("/games", getGame)
+
+	/* /cards */
+	r.GET("/cards", getCard)
 }
